5bd36dadc012bd255f6598218648e10aa24537e4
kasspablo <kasspablo@gmail.com>
2023-10-18 00:45:37 -0300

    Added area model

---
+++ packages/backend/src/database/models/area.ts
@@ -0,0 +1,57 @@
+import { Model, DataTypes } from 'sequelize'
+import { sequelize } from '..'
+
+export class Area extends Model {
+  public id!: number
+  public name!: string
+  public rolename!: string
+  public description!: string
+  public multiple!: boolean
+  public parentId!: number
+  public responsibleId!: number
+
+  public createdAt!: Date
+  public updatedAt!: Date
+  public deletedAt!: Date
+
+}
+
+Area.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rolename: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    multiple: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    parentId: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+    responsibleId: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Area',
+    tableName: 'areas',
+    timestamps: true,
+    paranoid: true,
+  }
+)
