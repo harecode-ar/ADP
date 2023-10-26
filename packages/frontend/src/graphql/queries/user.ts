8e00bf741db31f506bff04ad2858b6489a5e7371
Andres <andresgomezic@gmail.com>
2023-10-26 11:14:59 -0300

    ADP-8

---
+++ packages/frontend/src/graphql/queries/user.ts
@@ -0,0 +1,37 @@
+import { gql } from '@apollo/client'
+
+export const USERS_FOR_LIST = gql`
+  query usersForList {
+    users {
+      id
+      firstname
+      lastname
+      email
+      telephone
+      password
+      roleId
+    }
+  }
+`
+export const USERS_FOR_SELECT = gql`
+  query usersForSelect {
+    users {
+      id
+      firstname
+      lastname
+    }
+  }
+`
+export const GET_USER = gql`
+  query user($id: Int!) {
+    user(id: $id) {
+      id
+      firstname
+      lastname
+      email
+      telephone
+      password
+      roleId
+    }
+  }
+`
