220f7e93e5148af9925638373b929825b10b94af
lucaslezano <lukitaslezano@gmail.com>
2023-11-07 14:22:11 -0300

    fix barProgressSelectedColor on dark

--- packages/frontend/src/sections/project/detalle/gantt-tab/gantt-component.tsx
+++ packages/frontend/src/sections/project/detalle/gantt-tab/gantt-component.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Task, ViewMode, Gantt } from 'gantt-task-react'
-import { Box } from '@mui/material'
+import { Box, Card } from '@mui/material'
 import { GREY } from 'src/theme/palette'
 import { useTheme, alpha } from '@mui/material/styles'
 import { ViewSwitcher } from './view-switcher'
@@ -33,25 +33,27 @@ const GanttComponent = (props: TProps) => {
   }
   const isDark = mode === 'dark'
   return (
-    <Box className={`gantt-container ${mode}`}>
-      <ViewSwitcher onViewModeChange={(viewMode) => setView(viewMode)} />
-      <Gantt
-        projectBackgroundColor={isDark ? PRIMARY.main : PRIMARY.main}
-        projectBackgroundSelectedColor={isDark ? PRIMARY.dark : PRIMARY.dark}
-        barProgressColor={isDark ? PRIMARY.main : PRIMARY.main}
-        barProgressSelectedColor={isDark ? PRIMARY.main : PRIMARY.dark}
-        barBackgroundColor={isDark ? GREY[600] : GREY[500]}
-        barBackgroundSelectedColor={isDark ? GREY[700] : GREY[600]}
-        todayColor={alpha(PRIMARY.lighter, isDark ? 0.15 : 0.35)}
-        tasks={tasks}
-        viewMode={view}
-        locale="es"
-        listCellWidth=""
-        columnWidth={columnWidth}
-        onClick={handleClick}
-        TooltipContent={TaskTooltip}
-      />
-    </Box>
+    <Card sx={{ p: 2 }}>
+      <Box className={`gantt-container ${mode}`}>
+        <ViewSwitcher onViewModeChange={(viewMode) => setView(viewMode)} />
+        <Gantt
+          projectBackgroundColor={isDark ? PRIMARY.main : PRIMARY.main}
+          projectBackgroundSelectedColor={isDark ? PRIMARY.dark : PRIMARY.dark}
+          barProgressColor={isDark ? PRIMARY.main : PRIMARY.main}
+          barProgressSelectedColor={isDark ? PRIMARY.dark : PRIMARY.dark}
+          barBackgroundColor={isDark ? GREY[600] : GREY[500]}
+          barBackgroundSelectedColor={isDark ? GREY[700] : GREY[600]}
+          todayColor={alpha(PRIMARY.lighter, isDark ? 0.15 : 0.35)}
+          tasks={tasks}
+          viewMode={view}
+          locale="es"
+          listCellWidth=""
+          columnWidth={columnWidth}
+          onClick={handleClick}
+          TooltipContent={TaskTooltip}
+        />
+      </Box>
+    </Card>
   )
 }
 
