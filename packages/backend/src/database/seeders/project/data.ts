053191607a2cf112829d374c4f459760f2e71fb1
Martin Facciuto <martin_facciuto@hotmail.com>
2023-11-22 16:52:07 -0300

    [+] format project & stages dates

--- packages/backend/src/database/seeders/project/data.ts
+++ packages/backend/src/database/seeders/project/data.ts
@@ -6,6 +6,27 @@ import { PROJECT as PROJECT5, STAGES as STAGES5 } from './5'
 import { PROJECT as PROJECT6, STAGES as STAGES6 } from './6'
 import { PROJECT as PROJECT7, STAGES as STAGES7 } from './7'
 
+const transformDate = (date: string, ref: string = '2023-11-15') => {
+  const d = new Date(date)
+  const r = new Date(ref)
+  const t = new Date()
+  let days = d.getDate() + (t.getDate() - r.getDate())
+  let months = d.getMonth() + (t.getMonth() - r.getMonth())
+  let years = d.getFullYear() + (t.getFullYear() - r.getFullYear())
+  if (days / 28 > 1) {
+    months += Math.round(days / 28)
+    days %= 28
+  }
+  if (months / 11 > 1) {
+    years += Math.round(months / 11)
+    months %= 11
+  }
+  d.setDate(days)
+  d.setMonth(months)
+  d.setFullYear(years)
+  return d.toISOString()
+}
+
 let id = 1
 const S = [STAGES1, STAGES2, STAGES3, STAGES4, STAGES5, STAGES6, STAGES7]
 export const STAGES = S.flatMap((stages, projectIndex) =>
@@ -14,6 +35,8 @@ export const STAGES = S.flatMap((stages, projectIndex) =>
     // eslint-disable-next-line no-plusplus
     id: id++,
     projectId: projectIndex + 1,
+    startDate: transformDate(stage.startDate),
+    endDate: transformDate(stage.endDate),
   }))
 )
 
@@ -25,5 +48,7 @@ export const PROJECTS = P.map((project, index) => {
     ...project,
     id: index + 1,
     progress,
+    startDate: transformDate(project.startDate),
+    endDate: transformDate(project.endDate),
   }
 })
