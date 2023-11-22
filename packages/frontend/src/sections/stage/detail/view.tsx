96ec59190fe6ddf85310cabc866ae8acc9838e5d
martinfacciuto <martin_facciuto@hotmail.com>
2023-11-21 21:39:18 -0300

    Fix tab selection in ProjectDetailView component

--- packages/frontend/src/sections/stage/detail/view.tsx
+++ packages/frontend/src/sections/stage/detail/view.tsx
@@ -40,7 +40,7 @@ export default function ProjectDetailView(props: TProps) {
   const settings = useSettingsContext()
   const { enqueueSnackbar } = useSnackbar()
   const router = useRouter()
-  const [tab, setTab] = useState<ETab>(ETab.SUB_STAGES)
+  const [tab, setTab] = useState<ETab>(ETab.NOTES)
 
   const stageQuery = useQuery(GET_STAGE, {
     variables: { id: Number(stageId) },
@@ -203,8 +203,8 @@ export default function ProjectDetailView(props: TProps) {
             >
               <Tabs value={tab} onChange={(e, v) => setTab(v)}>
                 <Tab label={ETab.NOTES} value={ETab.NOTES} />
-                <Tab label={ETab.SUB_STAGES} value={ETab.SUB_STAGES} />
-                <Tab label={ETab.GANTT} value={ETab.GANTT} />
+                <Tab label={ETab.SUB_STAGES} value={ETab.SUB_STAGES} disabled />
+                <Tab label={ETab.GANTT} value={ETab.GANTT} disabled />
               </Tabs>
             </Card>
             {tab === ETab.NOTES && <NotesTab stage={stage} />}
@@ -213,8 +213,6 @@ export default function ProjectDetailView(props: TProps) {
               <SubStagesTab stage={stage} stages={stages} refetch={subStageQuery.refetch} />
             )}
             {tab === ETab.GANTT && <GanttTab stage={stage} stages={stages} />} */}
-            {tab === ETab.SUB_STAGES && <div>SubStagesTab</div>}
-            {tab === ETab.GANTT && <div>GanttTab</div>}
           </React.Fragment>
         )}
       </Box>
