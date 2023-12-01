ffeda6f1138c3b1fa063a6cf333406bba46bf8e2
martinfacciuto <martin_facciuto@hotmail.com>
2023-12-01 15:08:14 -0300

    Update favicon and metadata

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -19,10 +19,10 @@ import { ApolloWrapper } from 'src/lib/apollo-provider'
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Minimal UI Kit',
+  title: 'Administrador de Proyectos',
   description:
-    'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
-  keywords: 'react,material,kit,application,dashboard,admin,template',
+    'Administrador de Proyectos',
+  keywords: 'Administrador de Proyectos',
   themeColor: '#000000',
   manifest: '/manifest.json',
   viewport: {
