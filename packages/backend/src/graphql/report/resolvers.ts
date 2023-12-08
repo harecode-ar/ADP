38878dead5bbd981a486cc6f9867255b39b5a6da
martinfacciuto <martin_facciuto@hotmail.com>
2023-12-08 11:57:51 -0300

    Add projectCountByState resolver

--- packages/backend/src/graphql/report/resolvers.ts
+++ packages/backend/src/graphql/report/resolvers.ts
@@ -34,5 +34,34 @@ export default {
         throw error
       }
     },
+    projectCountByState: async (
+      _: any,
+      args: {
+        areas: number[]
+      }
+    ): Promise<IProjectCountByState> => {
+      try {
+        const { areas } = args
+
+        const query = `
+          SELECT
+            COUNT(CASE WHEN stateId = ${PROJECT_STATE.NEW} THEN 1 END) AS new,
+            COUNT(CASE WHEN stateId = ${PROJECT_STATE.IN_PROGRESS} THEN 1 END) AS inProgress,
+            COUNT(CASE WHEN stateId = ${PROJECT_STATE.COMPLETED} THEN 1 END) AS completed,
+            COUNT(CASE WHEN stateId = ${PROJECT_STATE.CANCELLED} THEN 1 END) AS cancelled
+          FROM projects
+          WHERE areaId IN (${areas.join(', ')})
+        `
+
+        const result = await sequelize.query(query, {
+          type: QueryTypes.SELECT,
+        })
+        // @ts-ignore
+        return result[0] as IProjectCountByState
+      } catch (error) {
+        logger.error(error)
+        throw error
+      }
+    },
   },
 }
