e2a7150febc3fd884c0b44dadce11ae371325e1e
martinfacciuto <martin_facciuto@hotmail.com>
2023-12-11 19:50:15 -0300

    Add CircularProgress component to InvoiceAnalytic

--- packages/frontend/src/sections/tablero/report-tab/component-one.tsx
+++ packages/frontend/src/sections/tablero/report-tab/component-one.tsx
@@ -1,8 +1,63 @@
 import React from 'react'
-import { Card, Stack, Divider, Typography, Box } from '@mui/material'
+import { Card, Stack, Divider, Typography, Box, CircularProgress } from '@mui/material'
 import Scrollbar from 'src/components/scrollbar'
-import { useTheme } from '@mui/material/styles'
-import InvoiceAnalytic from './invoice-analytic'
+import { alpha, useTheme } from '@mui/material/styles'
+
+// ----------------------------------------------------------------------
+
+type InvoiceAnalyticProps = {
+  amount: number
+  subtitle: string
+  percent: number
+  color?: string
+}
+
+function InvoiceAnalytic(props: InvoiceAnalyticProps) {
+  const { amount, subtitle, color, percent } = props
+  return (
+    <Stack
+      spacing={2.5}
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ width: 1, minWidth: 200 }}
+    >
+      <Stack alignItems="center" justifyContent="center" sx={{ position: 'relative' }}>
+        <Box sx={{ position: 'absolute', fontSize: 12 }}>{percent}%</Box>
+
+        <CircularProgress
+          variant="determinate"
+          value={percent}
+          size={56}
+          thickness={2}
+          sx={{ color, opacity: 0.48 }}
+        />
+
+        <CircularProgress
+          variant="determinate"
+          value={100}
+          size={56}
+          thickness={3}
+          sx={{
+            top: 0,
+            left: 0,
+            opacity: 0.48,
+            position: 'absolute',
+            color: (theme) => alpha(theme.palette.grey[500], 0.16),
+          }}
+        />
+      </Stack>
+
+      <Stack spacing={0.5}>
+        <Typography variant="subtitle1">{amount}</Typography>
+
+        <Box component="span" sx={{ color: 'text.disabled', typography: 'body2' }}>
+          {subtitle}
+        </Box>
+      </Stack>
+    </Stack>
+  )
+}
 
 type TProps = {
   newProjects: number
