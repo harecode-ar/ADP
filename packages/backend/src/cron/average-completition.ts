1b62357f8d256ea4c22488d5acbd6e58021fb75b
martinfacciuto <martin_facciuto@hotmail.com>
2024-01-04 15:17:42 -0300

    Add configuration and logic for sending alerts based on project deadlines and completion percentages

--- packages/backend/src/cron/average-completition.ts
+++ packages/backend/src/cron/average-completition.ts
@@ -1,5 +1,5 @@
 import cron from 'node-cron'
-import { PROJECT_STATE, STAGE_STATE } from '@adp/shared'
+import { PROJECT_STATE, STAGE_STATE, EConfigurationKey } from '@adp/shared'
 import { sequelize } from '../database'
 import {
   Project,
@@ -10,8 +10,10 @@ import {
   UserAverageCompletition,
   Area,
   AreaAverageCompletition,
+  Configuration,
 } from '../database/models'
 import { getAcp } from '../utils/average-completition'
+import { getDaysDiff } from '../utils/date'
 
 // Run every dat at 06:30 AM (UTC-3)
 // Run every dat at 09:30 AM (UTC)
@@ -161,4 +163,57 @@ cron.schedule('30 6 * * *', async () => {
 
 // Run every dat at 06:35 AM (UTC-3)
 // Run every dat at 09:35 AM (UTC)
-cron.schedule('35 6 * * *', async () => {})
+cron.schedule('35 6 * * *', async () => {
+  const configuration = await Configuration.findOne({
+    where: { key: EConfigurationKey.PERCENTAGE_ALERT_MARGIN },
+  })
+
+  const percentageAlertMargin = configuration ? Number(configuration.value) : 0
+
+  const [projects, stages] = await Promise.all([
+    Project.findAll({
+      attributes: ['id', 'startDate', 'endDate', 'finishedAt'],
+      where: {
+        stateId: PROJECT_STATE.IN_PROGRESS,
+      },
+    }),
+    Stage.findAll({
+      attributes: ['id', 'startDate', 'endDate', 'finishedAt'],
+      where: {
+        stateId: STAGE_STATE.IN_PROGRESS,
+      },
+    }),
+  ])
+
+  await Promise.all([
+    ...projects.map((project) => {
+      const { startDate, endDate } = project
+      const currentDate = new Date().toISOString().split('T')[0]
+      const days = getDaysDiff(startDate, endDate)
+
+      const expectedDays = Math.round(days * percentageAlertMargin) || 1
+      const currentDays = getDaysDiff(currentDate, endDate)
+
+      if (currentDays === 0) {
+        console.log(`Alerta, el plazo finaliza hoy`)
+      } else if (currentDays < 0) {
+        console.log(`Alerta, el plazo vencio hace ${Math.abs(currentDays)} dias`)
+      } else if (expectedDays >= currentDays) {
+        console.log(`Alerta, faltan ${Math.abs(currentDays)} dias para que finalize el plazo`)
+      }
+
+      if (currentDate >= endDate) {
+        // expired
+      } else {
+        // not expired
+      }
+      return project.save()
+    }),
+    ...stages.map((stage) => {
+      const { acp, pacp } = getAcp(stage)
+      stage.acp = acp
+      stage.pacp = pacp
+      return stage.save()
+    }),
+  ])
+})
