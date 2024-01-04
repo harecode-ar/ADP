479caad4c649daae418dd3988cd7fde44e1da59d
lucaslezano <lukitaslezano@gmail.com>
2024-01-04 12:57:57 -0300

    ADP-231

--- packages/frontend/src/sections/tablero/assignment-tab/index.tsx
+++ packages/frontend/src/sections/tablero/assignment-tab/index.tsx
@@ -2,14 +2,32 @@
 
 import { IProject, IStage } from '@adp/shared'
 import React, { useState, useMemo } from 'react'
-import { Box, Stack, TextField, InputAdornment, Card } from '@mui/material'
+import { Box, Stack, TextField, InputAdornment, Card, Grid, Autocomplete } from '@mui/material'
 import { GET_USER_ASSIGNMENTS } from 'src/graphql/queries'
 import { useQuery } from '@apollo/client'
 import Iconify from 'src/components/iconify'
 import AssignmentSection from './assignment-section'
 
+enum EOption {
+  ALL = 'Todos',
+  PROJECT = 'Proyectos',
+  STAGE = 'Etapas',
+  SUB_STAGE = 'Sub etapas'
+}
+
+const OPTIONS = [EOption.ALL, EOption.PROJECT, EOption.STAGE, EOption.SUB_STAGE]
+
 export default function AssignmentTab() {
   const [search, setSearch] = useState('')
+  const [selectedOptions, setSelectedOptions] = useState<EOption[]>([EOption.ALL])
+
+  const handleChangeOptions = (event: React.ChangeEvent<{}>, newValue: EOption[]) => {
+    if (EOption.ALL === newValue[newValue.length - 1]) {
+      setSelectedOptions([EOption.ALL])
+    } else {
+      setSelectedOptions(newValue.filter(option => option !== EOption.ALL ))
+    }
+  }
 
   const handleSearch = (event: any) => {
     const { value } = event.target
@@ -50,25 +68,48 @@ export default function AssignmentTab() {
   }, [assignmentQuery.data])
 
   const filteredProjects = useMemo(
-    () => projects.filter((project) => project.name.toLowerCase().includes(search.toLowerCase())),
-    [projects, search]
+    () => projects.filter((project) => (selectedOptions.includes(EOption.ALL) || selectedOptions.includes(EOption.PROJECT)) && project.name.toLowerCase().includes(search.toLowerCase())),
+    [projects, search, selectedOptions]
   )
 
   const filteredStages = useMemo(
-    () => stages.filter((stage) => stage.name.toLowerCase().includes(search.toLowerCase())),
-    [stages, search]
+    () => stages.filter((stage) => (selectedOptions.includes(EOption.ALL) || selectedOptions.includes(EOption.STAGE)) && stage.name.toLowerCase().includes(search.toLowerCase())),
+    [stages, search, selectedOptions]
   )
 
   const filteredSubStages = useMemo(
     () =>
-      subStages.filter((subStage) => subStage.name.toLowerCase().includes(search.toLowerCase())),
-    [subStages, search]
+      subStages.filter((subStage) => (selectedOptions.includes(EOption.ALL) || selectedOptions.includes(EOption.SUB_STAGE)) && subStage.name.toLowerCase().includes(search.toLowerCase())),
+    [subStages, search, selectedOptions]
   )
+
 
   return (
     <Card sx={{ p: 2 }}>
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-        <Stack spacing={2} justifyContent="end" direction={{ xs: 'column', sm: 'row' }}>
+        <Stack spacing={2} justifyContent="space-between" direction={{ xs: 'column', sm: 'row' }}>
+        <Stack spacing={2} direction={{ xs: 'column', sm: 'row' }}>
+          <Autocomplete
+              multiple
+              options={OPTIONS}
+              getOptionLabel={(option) => option}
+              renderInput={(params) => <TextField {...params} label="Tipos" />}
+              noOptionsText="No hay tipos"
+              disableClearable
+              value={selectedOptions}
+              onChange={handleChangeOptions}
+            />
+            <Autocomplete
+              multiple
+              options={OPTIONS}
+              getOptionLabel={(option) => option}
+              renderInput={(params) => <TextField {...params} label="Estado" />}
+              noOptionsText="No hay estados"
+              disableClearable
+              value={selectedOptions}
+              onChange={handleChangeOptions}
+            />
+            </Stack>
           <TextField
             value={search}
             onChange={handleSearch}
@@ -83,12 +124,14 @@ export default function AssignmentTab() {
             sx={{ width: { xs: 1, sm: 260 } }}
           />
         </Stack>
+
         <AssignmentSection
           projects={filteredProjects}
           stages={filteredStages}
           subStages={filteredSubStages}
         />
       </Box>
+
     </Card>
   )
 }
