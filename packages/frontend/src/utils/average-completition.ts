36e61694b5c41a96c6952e82ee11697203888829
kasspablo <kasspablo@gmail.com>
2024-01-09 17:36:45 -0300

    Add acp and pacp fields to detail project and stage

--- packages/frontend/src/utils/average-completition.ts
+++ packages/frontend/src/utils/average-completition.ts
@@ -35,3 +35,39 @@ export const getColorFromPacp = (pacp: number | null, percentageAlertMargin: num
 
   return GREY[500]
 }
+
+export const getTooltipFromAcp = (acp: number | null, percentageAlertMargin: number) => {
+  if (acp === null) {
+    return 'Aun no ha comenzado'
+  }
+
+  if (acp <= 0) {
+    return 'Finalizado a tiempo'
+  }
+  if (percentageAlertMargin >= acp) {
+    return 'Finalizado con retraso'
+  }
+  if (acp > percentageAlertMargin) {
+    return 'Finalizado con mucho retraso'
+  }
+
+  return ''
+}
+
+export const getTooltipFromPacp = (pacp: number | null, percentageAlertMargin: number) => {
+  if (pacp === null) {
+    return 'Aun no ha comenzado'
+  }
+
+  if (pacp + percentageAlertMargin <= 0) {
+    return 'Dentro de los plazos previstos'
+  }
+  if (pacp <= 0 && pacp > -percentageAlertMargin) {
+    return 'Por finalizar'
+  }
+  if (pacp > 0) {
+    return 'Fuera de plazo'
+  }
+
+  return ''
+}
