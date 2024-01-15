3885804d9b576e2648500245bf0ae221017fae73
Martin Facciuto <martin_facciuto@hotmail.com>
2024-01-15 20:41:38 -0300

    Update project-tab and project-line components

--- packages/frontend/src/sections/area/detalle/project-tab/index.tsx
+++ packages/frontend/src/sections/area/detalle/project-tab/index.tsx
@@ -1,5 +1,5 @@
 import { IProject, PROJECT_STATE_ARRAY } from '@adp/shared'
-import { Box, Autocomplete, TextField, Typography, Divider } from '@mui/material'
+import { Box, Autocomplete, TextField, Typography, Divider, Card } from '@mui/material'
 import { GET_PROJECTS_BY_AREA_AND_STATE } from 'src/graphql/queries'
 import { useQuery } from '@apollo/client'
 import React, { useState, useMemo } from 'react'
@@ -39,40 +39,46 @@ export default function ProjectTab(props: TProps) {
         gap: 2,
       }}
     >
-      <Box className="ViewContainer">
-        <Autocomplete
-          noOptionsText="No hay estados"
-          style={{ width: 170, marginBottom: '16px' }}
-          options={[{ id: 0, name: 'Todos' }, ...PROJECT_STATE_ARRAY]}
-          getOptionLabel={(option) => option.name}
-          value={viewOption}
-          onChange={handleViewModeChange}
-          renderInput={(params) => <TextField {...params} label="Estado" />}
-          clearIcon={null}
-        />
-      </Box>
-      {projects.length === 0 ? (
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-            marginTop: '16px',
-          }}
-        >
-          <Typography>No hay proyectos asignados en ese estado en esta area.</Typography>
+      <Card sx={{ p: 2 }}>
+        <Box className="ViewContainer">
+          <Autocomplete
+            noOptionsText="No hay estados"
+            style={{ width: 170, marginBottom: '16px' }}
+            options={[{ id: 0, name: 'Todos' }, ...PROJECT_STATE_ARRAY]}
+            getOptionLabel={(option) => option.name}
+            value={viewOption}
+            onChange={handleViewModeChange}
+            renderInput={(params) => <TextField {...params} label="Estado" />}
+            clearIcon={null}
+          />
         </Box>
-      ) : (
-        <React.Fragment>
-          {projects.map((project) => (
-            <React.Fragment key={project.id}>
-              <ProjectLine project={project} />
-              <Divider />
-            </React.Fragment>
-          ))}
-        </React.Fragment>
-      )}
+        {projects.length === 0 ? (
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100%',
+              marginTop: '16px',
+            }}
+          >
+            <Typography>No hay proyectos asignados en ese estado en esta area.</Typography>
+          </Box>
+        ) : (
+          <React.Fragment>
+            {projects.map((project, index) => (
+              <React.Fragment key={project.id}>
+                {index !== 0 && (
+                  <Divider
+                    sx={{ mt: 2, mb: 1, borderBottomWidth: 2, borderBottomStyle: 'dotted' }}
+                  />
+                )}
+                <ProjectLine project={project} />
+              </React.Fragment>
+            ))}
+          </React.Fragment>
+        )}
+      </Card>
     </Box>
   )
 }
--- packages/frontend/src/sections/area/detalle/project-tab/project-line.tsx
+++ packages/frontend/src/sections/area/detalle/project-tab/project-line.tsx
@@ -41,6 +41,7 @@ export default function ProjectLine(props: TProps) {
           display: 'flex',
           flexDirection: 'row',
           alignItems: 'center',
+          justifyContent: 'center',
           gap: 1,
         }}
       >
@@ -52,7 +53,15 @@ export default function ProjectLine(props: TProps) {
             backgroundColor: colorFromAcpOrPacp(project),
           }}
         />
-        <Typography>{project.name}</Typography>
+        <Typography
+          sx={{
+            fontSize: '18px',
+            textAlign: 'center',
+          }}
+        >
+          {project.name}
+        </Typography>
+
         <Link
           component={NextLink}
           href={paths.dashboard.project.detail.replace(':id', String(project.id))}
@@ -90,15 +99,59 @@ export default function ProjectLine(props: TProps) {
             overflow: 'hidden',
           }}
         >
-          {project.stages.map((stage, index) => (
-            <StageSubLine
-              key={stage.id}
-              project={project}
-              stage={stage}
-              first={index === 0}
-              last={index === project.stages.length - 1}
-            />
-          ))}
+          {expanded &&
+            project.stages.map((stage, index) => (
+              <StageSubLine
+                key={stage.id}
+                project={project}
+                stage={stage}
+                first={index === 0}
+                last={index === project.stages.length - 1}
+              />
+            ))}
+          {!expanded && (
+            <Box
+              sx={(theme) => ({
+                width: '100%',
+                backgroundColor: theme.palette.mode === 'dark' ? 'grey.700' : 'grey.300',
+                height: 35,
+                borderRadius: 2,
+                position: 'relative',
+              })}
+            >
+              <Box
+                sx={{
+                  width: `${project.progress * 100}%`,
+                  backgroundColor: colorFromAcpOrPacp(project),
+                  height: 35,
+                  borderRadius: 2,
+                  textAlign: 'center',
+                }}
+              />
+              <Box
+                sx={{
+                  width: '100%',
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  position: 'absolute',
+                  top: 0,
+                }}
+              >
+                <Typography
+                  sx={{
+                    color: 'white',
+                    fontWeight: 'bold',
+                    lineHeight: '35px',
+                    textShadow: '0px 0px 2px rgba(0,0,0,0.5)',
+                    fontSize: '18px',
+                  }}
+                >
+                  {project.progress * 100}%
+                </Typography>
+              </Box>
+            </Box>
+          )}
         </Box>
       </Box>
       {expanded && (
@@ -106,14 +159,18 @@ export default function ProjectLine(props: TProps) {
           sx={{
             display: 'flex',
             flexDirection: 'column',
-            gap: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 2,
+            mt: 1,
           }}
         >
           {project.stages.map((stage) => (
             <Box
               key={stage.id}
               sx={{
-                marginLeft: 6,
+                width: '90%',
+                marginLeft: '44px',
               }}
             >
               <StageLine stage={stage} />
