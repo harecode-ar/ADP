f3a111973f33a2117033002e15969d16f8cbe612
kasspablo <kasspablo@gmail.com>
2024-01-23 16:25:34 -0300

    new order for the substages

--- packages/backend/src/graphql/stage/resolvers.ts
+++ packages/backend/src/graphql/stage/resolvers.ts
@@ -216,7 +216,10 @@ export default {
               as: 'state',
             },
           ],
-          order: [['startDate', 'ASC']],
+          order: [
+            ['startDate', 'ASC'],
+            ['createdAt', 'ASC'],
+          ],
         }) as unknown as Promise<Omit<IStage, 'parentStage' | 'childStages' | 'project'>[]>
       } catch (error) {
         logger.error(error)
