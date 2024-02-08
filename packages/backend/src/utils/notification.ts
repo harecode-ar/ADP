4cfc265fdf046e3895bc4c60e0c42fef8b1438de
Martin Facciuto <martin_facciuto@hotmail.com>
2024-02-07 22:21:32 -0300

    Refactor notification creation and use sendNotification utility

---
+++ packages/backend/src/utils/notification.ts
@@ -0,0 +1,39 @@
+import { ENotificationCategory, INotification } from '@adp/shared'
+import { Notification, User, UserNotification } from '../database/models'
+import { sendNotificationMail } from '../services/nodemailer/notification'
+
+interface TSettings {
+  title: string
+  category: ENotificationCategory
+  userIds: number[]
+  email?: boolean
+}
+
+export const sendNotification = async (settings: TSettings) => {
+  const { title, category, userIds, email = false } = settings
+
+  const notification = await Notification.create({
+    title,
+    category,
+  })
+
+  const users = await User.findAll({
+    where: {
+      id: userIds,
+    },
+    attributes: ['id', 'email'],
+  })
+
+  await UserNotification.bulkCreate(
+    users.map((user) => ({
+      userId: user.id,
+      notificationId: notification.id,
+    }))
+  )
+
+  if (email) {
+    users.forEach((user) => {
+      sendNotificationMail(user, notification as INotification)
+    })
+  }
+}
